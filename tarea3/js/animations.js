a5cc63f023bfa19cb2da6100245a53a74207d64c
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2024-06-26 22:53:17 -0600

    tarea con banner completo y seccion de menu con respinsividad

--- tarea3/js/animations.js
+++ tarea3/js/animations.js
@@ -1,4 +1,28 @@
-gsap.timeline()
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+gsap.fromTo(".mcLogoSvg",{ opacity: 0},
+    { opacity: 1, duration: 5 })   
+
+
+
+
+
+let myMediaQuery = window.matchMedia('(min-width: 1440px)');
+
+if(myMediaQuery.matches) {
+    gsap.timeline()
     .to(".red-load", {
         duration: 4,
         width: "200vh",
@@ -7,7 +31,28 @@ gsap.timeline()
     })
     .to(".banner", {
         backgroundColor: "red",
-        duration: 1
+        duration: 2
     }, "-=1.5")
-   
-
+    
+    gsap.to(".text-go-menu", { scale: 0.9,
+        duration: 1.5, 
+        repeat: -1,   
+        yoyo: true });
+   } else{
+    gsap.timeline()
+    .to(".red-load", {
+        duration: 10,
+        width: "200vh",
+        height: "200vh",
+        ease: "power2.out"
+    })
+    .to(".banner", {
+        backgroundColor: "red",
+        duration: 2
+    }, "-=1.5")
+    
+    gsap.to(".text-go-menu", { scale: 0.9,
+        duration: 1.5, 
+        repeat: -1,   
+        yoyo: true });
+   }
