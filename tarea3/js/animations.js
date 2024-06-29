9fdd777eba4d427bc9fb6f71feccaf90fd02fe69
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2024-06-28 23:18:44 -0600

    animaciones para menu items lista con queries

--- tarea3/js/animations.js
+++ tarea3/js/animations.js
@@ -12,6 +12,7 @@ function raf(time) {
 requestAnimationFrame(raf)
 
 
+
 gsap.fromTo(".mcLogoSvg",{ opacity: 0},
     { opacity: 1, duration: 5 })   
 
@@ -19,40 +20,78 @@ gsap.fromTo(".mcLogoSvg",{ opacity: 0},
 
 
 
-let myMediaQuery = window.matchMedia('(min-width: 1440px)');
+    let myMediaQuery = window.matchMedia('(min-width: 1440px)');
 
-if(myMediaQuery.matches) {
-    gsap.timeline()
-    .to(".red-load", {
-        duration: 4,
-        width: "200vh",
-        height: "200vh",
-        ease: "power2.out"
-    })
-    .to(".banner", {
-        backgroundColor: "red",
-        duration: 2
-    }, "-=1.5")
+    if (myMediaQuery.matches) {
+        
+        let tl1 = gsap.timeline()
+            .to(".red-load", {
+                duration: 4,
+                width: "200vh",
+                height: "200vh",
+                ease: "power2.out"
+            })
+            .to(".banner", {
+                backgroundColor: "red",
+                duration: 2
+            }, "-=1.5");
+        
+        gsap.to(".text-go-menu", { 
+            scale: 0.9,
+            duration: 1.5, 
+            repeat: -1,   
+            yoyo: true 
+        });
+        document.querySelectorAll('.menu-item').forEach(item => {
+            let scrollTimeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: item,
+                    start: 'top 99%',
+                    end: 'top center',
+                    scrub: false,
+                    
+                }
+            });
+            scrollTimeline.to(item, {
+                x: 5000,
+                duration: 3
+            });
+        });
+    } else {
+       
+        let tl2 = gsap.timeline()
+            .to(".red-load", {
+                duration: 10,
+                width: "200vh",
+                height: "200vh",
+                ease: "power2.out"
+            })
+            .to(".banner", {
+                backgroundColor: "red",
+                duration: 2
+            }, "-=1.5");
+        
+        gsap.to(".text-go-menu", { 
+            scale: 0.9,
+            duration: 1.5, 
+            repeat: -1,   
+            yoyo: true 
+        });
+
+        document.querySelectorAll('.menu-item').forEach(item => {
+            let scrollTimeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: item,
+                    start: 'top 99%',
+                    end: 'top center',
+                    scrub: false,
+                }
+            });
+            scrollTimeline.to(item, {
+                x: 400,
+                duration: 2
+            });
+        });
+    }
+       
     
-    gsap.to(".text-go-menu", { scale: 0.9,
-        duration: 1.5, 
-        repeat: -1,   
-        yoyo: true });
-   } else{
-    gsap.timeline()
-    .to(".red-load", {
-        duration: 10,
-        width: "200vh",
-        height: "200vh",
-        ease: "power2.out"
-    })
-    .to(".banner", {
-        backgroundColor: "red",
-        duration: 2
-    }, "-=1.5")
-    
-    gsap.to(".text-go-menu", { scale: 0.9,
-        duration: 1.5, 
-        repeat: -1,   
-        yoyo: true });
-   }
